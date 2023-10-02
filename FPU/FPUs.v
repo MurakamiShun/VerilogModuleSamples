849b14a2629acc2e3cf0382a63a317039a6bfee6
MURAKAMI <enshuuritsu.about3@gmail.com>
2023-10-02 15:51:14 +0900

    add UART timing charts

--- FPU/FPUs.v
+++ FPU/FPUs.v
@@ -3,7 +3,7 @@
 `define ROUND_DOWNWARD 2'b10
 `define ROUND_UPWARD 2'b11
 
-module F32Rounding(
+module F32_round(
     input wire sign,
     input wire[26:0] in_frac,
     input wire[1:0] mode,
@@ -16,11 +16,10 @@ module F32Rounding(
     //wire S = in_frac[0];
     wire[24:0] norm_in_frac = {1'b0, in_frac[26:3]};
 
-    wire[24:0] TONEAREST = (in_frac[2] & ((in_frac[3] & ~|in_frac[1:0]) | (|in_frac[1:0]))) ? (norm_in_frac+25'h1) :
-        norm_in_frac;
+    wire[24:0] TONEAREST = norm_in_frac+(in_frac[2] & ((in_frac[3] & ~|in_frac[1:0]) | (|in_frac[1:0])));
     wire[24:0] TOWARDZERO = norm_in_frac;
-    wire[24:0] DOWNWARD = sign ? (norm_in_frac+(|in_frac[2:0])) : norm_in_frac;
-    wire[24:0] UPWARD = sign ? norm_in_frac : (norm_in_frac+(|in_frac[2:0]));
+    wire[24:0] DOWNWARD = norm_in_frac+(sign & |in_frac[2:0]);
+    wire[24:0] UPWARD = norm_in_frac+(~sign & |in_frac[2:0]);
 
     wire[24:0] tmp_frac = (mode == `ROUND_TOWARDZERO) ? TOWARDZERO:
         (mode == `ROUND_DOWNWARD) ? DOWNWARD:
@@ -32,7 +31,10 @@ module F32Rounding(
 
 endmodule
 
-module F32Multiplier(
+module F32_mul(
+    input wire CLK,
+    input wire RST,
+    input wire en,
     input wire[31:0] op1,
     input wire[31:0] op2,
     input wire[1:0] round_mode,
@@ -50,15 +52,17 @@ module F32Multiplier(
     wire[22:0] op1_frac = op1[22:0];
     wire[22:0] op2_frac = op2[22:0];
 
-    wire[47:0] frac_mul = {1'b1, op1_frac} * {1'b1, op2_frac};
+    wire[47:0] frac_mul = {1'b1, op1_frac} * {1'b1, op2_frac}; 
+    
     wire[26:0] norm_frac_mul = frac_mul[47] ? {frac_mul[47:22], |frac_mul[21:0]} : {frac_mul[46:21], |frac_mul[20:0]};
+    
     wire mul_carry = frac_mul[47];
 
     wire result_sign = op1_sign ^ op2_sign;
     wire[23:0] round_frac;
     wire round_carry;
 
-    F32Rounding round_module(
+    F32_round round_module(
         .sign(result_sign),
         .in_frac(norm_frac_mul),
         .mode(round_mode),
@@ -67,7 +71,7 @@ module F32Multiplier(
         .carry(round_carry)
     );
 
-    wire[7:0] result_exp = op1_exp + op2_exp - 8'd127 + round_carry + mul_carry;
+    wire[7:0] result_exp = (op1_exp + op2_exp - 8'd127) + (round_carry + mul_carry);
 
     wire is_zero = (op1_exp == 0 && (~|op1_frac) || op2_exp == 0 && (~|op2_frac));
 
@@ -75,7 +79,7 @@ module F32Multiplier(
         {result_sign, result_exp, round_frac[22:0]};
 endmodule
 
-module F32Adder(
+module F32_add(
     input wire[31:0] op1,
     input wire[31:0] op2,
     input wire[1:0] round_mode,
@@ -100,7 +104,7 @@ module F32Adder(
 
     wire[49:0] add_frac = (op_big_sign ? -op_big_frac : op_big_frac) + (op_small_sign ? -op_small_frac : op_small_frac);
     wire[48:0] add_abs_frac = add_frac[49] ? -add_frac : add_frac;
-    
+
     wire[48:0] add_norm_frac6 = norm_shift[5] ? (add_abs_frac << 32) : add_abs_frac;
     wire[48:0] add_norm_frac5 = norm_shift[4] ? (add_norm_frac6 << 16) : add_norm_frac6;
     wire[48:0] add_norm_frac4 = norm_shift[3] ? (add_norm_frac5 << 8) : add_norm_frac5;
@@ -117,7 +121,7 @@ module F32Adder(
     };
     wire[23:0] round_frac;
 
-    F32Rounding round_module(
+    F32_round round_module(
         .sign(add_frac[49]),
         .in_frac({add_norm_frac1[48:23], |add_norm_frac1[22:0]}),
         .mode(round_mode),
@@ -125,10 +129,48 @@ module F32Adder(
         .out_frac(round_frac),
         .carry(round_carry)
     );
-    wire[7:0] result_exp = op_big_exp - ({2'b0,norm_shift}-8'h1) + round_carry;
+    wire[7:0] result_exp = op_big_exp - norm_shift + (round_carry+1);
 
     assign result = (~|add_abs_frac) ? {op1[31] & op2[31], 31'h0}:
         (~|op1[30:0]) ? op2:
         (~|op2[30:0]) ? op1:
         {add_frac[49], result_exp, round_frac[22:0]};
+endmodule
+
+`define FPU_ADD 1
+`define FPU_MUL 2
+
+module FPU(
+    input wire CLK,
+    input wire RST,
+    input wire[31:0] op1,
+    input wire[31:0] op2,
+    input wire[31:0] op3,
+    input wire[4:0] funct,
+    input wire[1:0] round_mode,
+
+    output wire[31:0] result
+);
+wire[31:0] mul_result, add_result;
+
+F32_mul f32_mul(
+    .op1(op1),
+    .op2(op2),
+    .round_mode(round_mode),
+
+    .result(mul_result),
+    .exception()
+);
+
+F32_add f32_add(
+    .op1(op1),
+    .op2(op2),
+    .round_mode(round_mode),
+
+    .result(add_result),
+    .exception()
+);
+
+assign result = (funct == `FPU_ADD) ? add_result : mul_result;
+
 endmodule
---
+++ simpleUART/README.md
@@ -0,0 +1,12 @@
+# Transmit Timing
+
+![Transmit Timing](transmit_timing.svg)
+
+
+# Recieve Timing
+
+![Recieve Timing](recieve_timing.svg)
+
+
+### Timing Chart Generator
+https://rawgit.com/osamutake/tchart-coffee/master/bin/editor-offline.html
