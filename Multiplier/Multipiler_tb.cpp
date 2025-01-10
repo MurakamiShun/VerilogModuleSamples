a2cd3733c5810ffb5daf5ba81b3faac76fd5b435
MURAKAMIShun <enshuuritsu.about3@gmail.com>
2025-01-10 16:05:27 +0900

    enrich tests

--- Multiplier/Multipiler_tb.cpp
+++ Multiplier/Multipiler_tb.cpp
@@ -11,52 +11,85 @@ int main(int argc, char **argv) {
     // Instance
     auto mul = std::make_unique<VMultiplier>();
 
+    const std::vector<std::tuple<uint64_t, uint32_t, uint32_t, bool, bool>> tests = {
+        std::make_tuple(0xffff00810000ff7fUL, 0xaaaaaaab, 0x0002fe7d, true, true),
+        std::make_tuple(0xffff00810000ff7fUL, 0x0002fe7d, 0xaaaaaaab, true, true),
+        std::make_tuple(0x0000000000000000UL, 0x00000000, 0xffff8000, true, true),
+        std::make_tuple(0x0000000000000000UL, 0x80000000, 0x00000000, true, true),
+        std::make_tuple(0xffffffffffffffffUL, 0xffffffff, 0x00000001, true, true),
+        std::make_tuple(0xffffffffffffffffUL, 0x00000001, 0xffffffff, true, true),
+
+        std::make_tuple(0x0001fefe0000ff7fUL, 0xaaaaaaab, 0x0002fe7d, false, false),
+        std::make_tuple(0x0001fefe0000ff7fUL, 0x0002fe7d, 0xaaaaaaab, false, false),
+        std::make_tuple(0x0000000000000000UL, 0x00000000, 0xffff8000, false, false),
+        std::make_tuple(0x0000000000000000UL, 0x80000000, 0x00000000, false, false),
+        std::make_tuple(0x00000000ffffffffUL, 0xffffffff, 0x00000001, false, false),
+        std::make_tuple(0x00000000ffffffffUL, 0x00000001, 0xffffffff, false, false),
+
+        std::make_tuple(0xffff00810000ff7fUL, 0xaaaaaaab, 0x0002fe7d, true, false),
+        std::make_tuple(0x0001fefe0000ff7fUL, 0x0002fe7d, 0xaaaaaaab, true, false),
+        std::make_tuple(0x0000000000000000UL, 0x00000000, 0xffff8000, true, false),
+        std::make_tuple(0x0000000000000000UL, 0x80000000, 0x00000000, true, false),
+        std::make_tuple(0xffffffffffffffffUL, 0xffffffff, 0x00000001, true, false),
+        std::make_tuple(0x00000000ffffffffUL, 0x00000001, 0xffffffff, true, false),
+
+        std::make_tuple(0x0001fefe0000ff7fUL, 0xaaaaaaab, 0x0002fe7d, false, true),
+        std::make_tuple(0xffff00810000ff7fUL, 0x0002fe7d, 0xaaaaaaab, false, true),
+        std::make_tuple(0x0000000000000000UL, 0x00000000, 0xffff8000, false, true),
+        std::make_tuple(0x0000000000000000UL, 0x80000000, 0x00000000, false, true),
+        std::make_tuple(0x00000000ffffffffUL, 0xffffffff, 0x00000001, false, true),
+        std::make_tuple(0xffffffffffffffffUL, 0x00000001, 0xffffffff, false, true),
+    };
 
     mul->rst_n = 0;
     mul->clk = 0;
+    mul->op1 = 0;
+    mul->op2 = 0;
+    mul->op1_sign = 0;
+    mul->op2_sign = 0;
     mul->eval();
     mul->clk = 1;
     mul->eval();
     
     mul->rst_n = 1;
-    mul->clk = 0;
-    mul->op1 = 0xffffffff80000000;
-    mul->op2 = 0xffffffffffff8000;
-    mul->op1_sign = 1;
-    mul->op2_sign = 0;
-    mul->eval();
-    mul->clk = 1;
-    mul->eval();
-    std::cout << std::hex << std::setw(8) << std::setfill('0') << mul->result[3] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[2] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[1] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[0] << std::endl;
-    mul->clk = 0;
-    mul->eval();
-    mul->clk = 1;
-    mul->eval();
-    std::cout << std::hex << std::setw(8) << std::setfill('0') << mul->result[3] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[2] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[1] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[0] << std::endl;
-    mul->clk = 0;
-    mul->eval();
-    mul->clk = 1;
-    mul->eval();
-    std::cout << std::hex << std::setw(8) << std::setfill('0') << mul->result[3] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[2] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[1] << "_"
-        << std::hex << std::setw(8) << std::setfill('0') << mul->result[0] << std::endl;
-    
-    // std::cout << std::hex << std::setw(16) << std::setfill('0') << mul->result << std::endl;
-    // mul->clk = 0;
-    // mul->eval();
-    // mul->clk = 1;
-    // mul->eval();
-    // std::cout << std::hex << std::setw(16) << std::setfill('0') << mul->result << std::endl;
-    // mul->clk = 0;
-    // mul->eval();
-    // mul->clk = 1;
-    // mul->eval();
-    // std::cout << std::hex << std::setw(16) << std::setfill('0') << mul->result << std::endl;
+    for(const auto [expected, op1, op2, op1_sign, op2_sign] : tests){
+        mul->clk = 0;
+        mul->op1 = op1;
+        mul->op2 = op2;
+        mul->op1_sign = op1_sign;
+        mul->op2_sign = op2_sign;
+        mul->eval();
+        mul->clk = 1;
+        mul->eval();
+        if(mul->result != 0){
+            std::cout << "latency miss match!!!" << std::hex << std::setw(16) << std::setfill('0') << mul->result << std::endl;
+            break;
+        }
+        
+        mul->op1 = 0;
+        mul->op2 = 0;
+        mul->op1_sign = 0;
+        mul->op2_sign = 0;
+        mul->clk = 0;
+        mul->eval();
+        mul->clk = 1;
+        mul->eval();
+        if(mul->result != 0){
+            std::cout << "latency miss match!!!" << std::hex << std::setw(16) << std::setfill('0') << mul->result << std::endl;
+            break;
+        }
+        mul->clk = 0;
+        mul->eval();
+        mul->clk = 1;
+        mul->eval();
+        if(mul->result != expected){
+            std::cout << "expected : " << std::hex << std::setw(16) << std::setfill('0') << expected << std::endl;
+            std::cout << "result   : " << std::hex << std::setw(16) << std::setfill('0') << mul->result << std::endl;
+            std::cout << "result   : " << std::hex << std::setw(8) << std::setfill('0') << op1 << (op1_sign ? " signed" : " unsigned") << std::endl;
+            std::cout << "result   : " << std::hex << std::setw(8) << std::setfill('0') << op2 << (op2_sign ? " signed" : " unsigned") << std::endl;
+            
+            break;
+        }
+    }
+    std::cout << "\u001b[32mtest passed!!" << std::endl;
 }
--- Multiplier/Multiplier.sv
+++ Multiplier/Multiplier.sv
@@ -1,6 +1,6 @@
 module Multiplier#(
-    parameter LATENCY = 2,
-    parameter WIDTH = 64
+    parameter LATENCY = 3,
+    parameter WIDTH = 32
 )(
     input logic clk,
     input logic rst_n,
@@ -17,6 +17,14 @@ module Multiplier#(
 logic[(WIDTH-1)*2-1:0] delay_reg[LATENCY-2:0];
 logic[WIDTH:0] sign_term[LATENCY-2:0];
 
+logic[WIDTH-2:0] op1_upper_term;
+logic[WIDTH-2:0] op2_upper_term;
+
+always_comb begin
+   op1_upper_term = {(WIDTH-1){op2_sign}} ^ ({(WIDTH-1){op2[WIDTH-1]}} & op1[WIDTH-2:0]);
+   op2_upper_term = {(WIDTH-1){op1_sign}} ^ ({(WIDTH-1){op1[WIDTH-1]}} & op2[WIDTH-2:0]);
+end
+
 always_ff@(posedge clk) begin
     if(!rst_n)begin
         for(integer i = 0; i <= LATENCY-2; i = i + 1)begin
@@ -25,21 +33,10 @@ always_ff@(posedge clk) begin
         end
     end else begin
         delay_reg[0] <= op1[WIDTH-2:0] * op2[WIDTH-2:0];
-        if(op1_sign && op2_sign)begin
-            sign_term[0] <=
-                  {1'b0, op1[WIDTH-1] & op2[WIDTH-1], op2[WIDTH-1] ? ~op1[WIDTH-2:0] : {(WIDTH-1){1'b1}}}
-                + {1'b0, 1'b0                       , op1[WIDTH-1] ? ~op2[WIDTH-2:0] : {(WIDTH-1){1'b1}}}
-                + {1'b1, {(WIDTH-2){1'b0}}, 2'b10};
-        end else if(!op1_sign && !op2_sign)begin
-            sign_term[0] <=
-                  {1'b0, op1[WIDTH-1] & op2[WIDTH-1], op2[WIDTH-1] ? op1[WIDTH-2:0] : {(WIDTH-1){1'b0}}}
-                + {1'b0, 1'b0                       , op1[WIDTH-1] ? op2[WIDTH-2:0] : {(WIDTH-1){1'b0}}};
-        end else begin
-            sign_term[0] <=
-                + {1'b0, ~(op1[WIDTH-1] & op2[WIDTH-1]), op2_sign ? (op2[WIDTH-1] ? ~op1[WIDTH-2:0] : {(WIDTH-1){1'b1}}) : (op2[WIDTH-1] ? op1[WIDTH-2:0] : {(WIDTH-1){1'b0}})}
-                + {1'b0, 1'b0,                           op1_sign ? (op1[WIDTH-1] ? ~op2[WIDTH-2:0] : {(WIDTH-1){1'b1}}) : (op1[WIDTH-1] ? op2[WIDTH-2:0] : {(WIDTH-1){1'b0}})}
-                + {1'b1, {(WIDTH-1){1'b0}}, 1'b1};
-        end
+        sign_term[0] <=
+            + {1'b0, (op1_sign ^ op2_sign) ^ (op1[WIDTH-1] & op2[WIDTH-1]), op1_upper_term}
+            + {1'b0, 1'b0                                                 , op2_upper_term}
+            + {op1_sign | op2_sign, {(WIDTH-2){1'b0}}, op1_sign & op2_sign, op1_sign ^ op2_sign};
         for(integer i = 1; i <= LATENCY-2; i = i + 1)begin
             delay_reg[i] <= delay_reg[i-1];
             sign_term[i] <= sign_term[i-1];
