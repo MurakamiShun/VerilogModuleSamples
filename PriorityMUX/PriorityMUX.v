d20a4b2298a283765e456eed268a9cde07fc4946
MurakamiShun <enshuuritsu.about3@gmail.com>
2025-12-13 14:35:11 +0900

    fix priority mux

--- PriorityMUX/PriorityMUX.v
+++ PriorityMUX/PriorityMUX.v
@@ -17,20 +17,19 @@ wire[WIDTH-1:0] tmp_data[INPUTSpadding*2-2:0];
 wire tmp_sel[INPUTSpadding*2-2:0];
 
 
-genvar i,n;
+genvar i;
 generate
-    for(i = 0; i < INPUTSpadding; i=i+1)begin : initial_loop
+    for(i = 0; i < INPUTSpadding*2-1; i=i+1)begin : priorily_mux_loop
         if(i < INPUTS)begin
             assign tmp_data[i] = i_data[i];
             assign tmp_sel[i]  = sel[i];
-        end else begin
+        end else if(i < INPUTSpadding) begin
             assign tmp_data[i] = 0;
             assign tmp_sel[i]  = 0;
+        end else begin
+            assign tmp_data[i] = tmp_sel[(i-INPUTSpadding)*2] ? tmp_data[(i-INPUTSpadding)*2] : tmp_data[(i-INPUTSpadding)*2+1];
+            assign tmp_sel[i]  = tmp_sel[(i-INPUTSpadding)*2] | tmp_sel[(i-INPUTSpadding)*2+1];
         end
-    end
-    for(i = 0; i < INPUTSpadding-1; i=i+1)begin : n_stage_loop
-        assign tmp_data[i+INPUTSpadding] = tmp_sel[i*2] ? tmp_data[i*2] : tmp_data[i*2+1];
-        assign tmp_sel[i+INPUTSpadding]  = tmp_sel[i*2] | tmp_sel[i*2+1];
     end
 endgenerate
 
